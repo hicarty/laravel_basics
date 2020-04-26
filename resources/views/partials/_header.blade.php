30af6d9e3a87d53b6ce7e6275cd306cb1ade1c18
Haven Carty <>
2020-04-26 18:21:53 +0100

    -ADDED: Bootstrap nav _header partial view

---
+++ resources/views/partials/_header.blade.php
@@ -0,0 +1,4 @@
+<nav class="nav justify-content-end">
+  <a class="nav-item nav-link active" href="#">Home</a>
+  <a class="nav-item nav-link disabled" href="#" tabindex="-1" aria-disabled="true">About</a>
+</nav>
